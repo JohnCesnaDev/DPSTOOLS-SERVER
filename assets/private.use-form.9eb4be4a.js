d142553add20e1f1df1a7d098e37318290a52e36
JohnCesnaDev <114229179+JohnCesnaDev@users.noreply.github.com>
2024-07-10 22:27:38 +0200

    Delete assets directory

--- assets/private.use-form.9eb4be4a.js
+++
@@ -1,1 +0,0 @@
-import{j as n,l as r}from"./index.04f822d4.js";const o={name:String};function s(e){return n(()=>({type:"hidden",name:e.name,value:e.modelValue}))}function c(e={}){return(t,u,a)=>{t[u](r("input",{class:"hidden"+(a||""),...e.value}))}}function i(e){return n(()=>e.name||e.for)}export{i as a,c as b,s as c,o as u};
